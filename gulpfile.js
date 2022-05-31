ed00a0725536e6d1ba6ec1b266fe9969c0b22ff7
Alina <nookimy@gmail.com>
2022-05-31 16:34:10 +0300

    Установка плагинов gulp-sass, sass, настройка

--- gulpfile.js
+++ gulpfile.js
@@ -38,7 +38,8 @@ let { src, dest } = require('gulp'),
     gulp = require ('gulp'),
     browsersync = require("browser-sync").create(),
     fileinclude = require("gulp-file-include"),
-    del = require("del");
+    del = require("del"),
+    scss = require('gulp-sass')(require('sass'));
 
 // Live-сервер для разработки
 function browserSync () {
@@ -59,9 +60,21 @@ function html() {
         .pipe(browsersync.stream())
 };
 
+function css() {
+    return src(path.src.css)
+        .pipe(
+            scss({
+                outputStyle: "expanded"
+            })
+        )
+        .pipe(dest(path.build.css))
+        .pipe(browsersync.stream())
+}
+
 // Слежка за изменениями в файлах
 function watchFiles () {
     gulp.watch([path.watch.html], html);
+    gulp.watch([path.watch.css], css);
 
     function clean () {
         return del(path.clean);
@@ -73,13 +86,14 @@ function clean () {
     return del(path.clean);
 };
 
-let build = gulp.series(clean, html);
+let build = gulp.series(clean, gulp.parallel(css, html));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
 
 
 
-exports.default = watch;
-exports.watch = watch;
-exports.build = build;
+exports.css = css;
 exports.html = html;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
