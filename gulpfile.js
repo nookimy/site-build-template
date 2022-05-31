a62a6334a0240a178d47c30489a0aa5efc72e956
Alina <nookimy@gmail.com>
2022-05-31 16:12:44 +0300

    Игнорирование файлов вложнных html-блоков при создании html-страниц в продакшене

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ let path = {
     },
     // Пути до исходников
     src: {
-        html: source_folder + "/*.html",
+        html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],
         css: source_folder + "/scss/style.scss",
         js:  source_folder + "/js/script.js",
         img:  source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
