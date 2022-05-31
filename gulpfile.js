343d3f5cfe3790e2245d640d6c791f44f34565ae
Alina <nookimy@gmail.com>
2022-05-31 15:25:32 +0300

    Объекты и переменные для работы с файлами

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+// Папка исходника
+let source_folder = "#src";
+
+// Папка продакшена
+let project_folder = "dist";
+
+// Пути к файлам и папкам
+let path = {
+    // Пути к папкам продакшена
+    build: {
+        html: project_folder + "/",
+        css: project_folder + "/css/",
+        js:  project_folder + "/js/",
+        img:  project_folder + "/img/",
+        fonts:  project_folder + "/fonts/",
+    },
+    // Пути до исходников
+    src: {
+        html: source_folder + "/",
+        css: source_folder + "/scss/style.scss",
+        js:  source_folder + "/js/script.js",
+        img:  source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+        fonts:  source_folder + "/fonts/*.ttf",
+    },
+    // Пути до файлов, за изменениями которых нужно следить
+    watch: {
+        html: source_folder + "/**/*.html",
+        css: source_folder + "/scss/**/*.scss",
+        js:  source_folder + "/js/**/*.js",
+        img:  source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+    },
+    // Путь к папке проекта, которую нужно удалять каждый раз при запуске
+    clean: "./" + project_folder + "/"
+};
