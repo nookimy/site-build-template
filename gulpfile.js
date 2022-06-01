004d5e88270c4dd0e6eb99b525ad028a00c2e855
Alina <nookimy@gmail.com>
2022-06-01 11:32:46 +0300

    Тест сборки единого js файла в продакшен

--- gulpfile.js
+++ gulpfile.js
@@ -65,6 +65,7 @@ function html() {
         .pipe(browsersync.stream())
 };
 
+// Обработка файлов стилей
 function css() {
     return src(path.src.css)
         .pipe(
@@ -93,11 +94,19 @@ function css() {
         .pipe(dest(path.build.css))
         .pipe(browsersync.stream())
 }
+// Обработка js-файлов
+function js() {
+    return src(path.src.js)
+        .pipe(fileinclude())
+        .pipe(dest(path.build.js))
+        .pipe(browsersync.stream())
+};
 
 // Слежка за изменениями в файлах
 function watchFiles () {
     gulp.watch([path.watch.html], html);
     gulp.watch([path.watch.css], css);
+    gulp.watch([path.watch.js], js);
 
     function clean () {
         return del(path.clean);
@@ -109,12 +118,12 @@ function clean () {
     return del(path.clean);
 };
 
-let build = gulp.series(clean, gulp.parallel(css, html));
+let build = gulp.series(clean, gulp.parallel(js, css, html));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
 
 
-
+exports.js = js;
 exports.css = css;
 exports.html = html;
 exports.build = build;
