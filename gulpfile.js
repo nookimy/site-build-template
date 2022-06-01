d1c2dfc091dbdf3daad02ab67c4e3cdc0c6aca41
Alina <nookimy@gmail.com>
2022-06-01 16:32:13 +0300

    Тестирование конвертации шрифтов из ttf в форматы woff

--- gulpfile.js
+++ gulpfile.js
@@ -49,7 +49,9 @@ let { src, dest } = require("gulp"),
     webp = require("gulp-webp"),
     webphtml = require("gulp-webp-html"),
     webpcss = require("gulp-webpcss"),
-    svgSprite = require("gulp-svg-sprite");
+    svgSprite = require("gulp-svg-sprite"),
+    ttf2woff = require("gulp-ttf2woff"),
+    ttf2woff2 = require("gulp-ttf2woff2");
 
 
 // Live-сервер для разработки
@@ -144,6 +146,16 @@ function images() {
         .pipe(browsersync.stream())
 };
 
+//Обработка шрифтов
+function fonts() {
+    src(path.src.fonts)
+        .pipe(ttf2woff())
+        .pipe(dest(path.build.fonts));
+    return src(path.src.fonts)
+        .pipe(ttf2woff2())
+        .pipe(dest(path.build.fonts));
+}
+
 //Создание svg-спрайта
 gulp.task("svgSprite", function () {
     return gulp.src([source_folder + "/iconsprite/*.svg"])
@@ -176,10 +188,10 @@ function clean () {
     return del(path.clean);
 };
 
-let build = gulp.series(clean, gulp.parallel(js, css, html, images));
+let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
-
+exports.fonts = fonts;
 exports.images = images;
 exports.js = js;
 exports.css = css;
