ceb9e498b8bff72100eea061a04bdaacba72e714
Alina <nookimy@gmail.com>
2022-06-02 15:27:45 +0300

    Подключение и тестирование слежки за файлами

--- gulpfile.js
+++ gulpfile.js
@@ -13,8 +13,16 @@ global.app = {
 // Импорт задач
 import { copy } from "./gulp/tasks/copy.js";
 
+// Наблюдатель за изменениями в файлах
+function watcher() {
+    gulp.watch(path.watch.files, copy);
+}
+
+// Построение сценариев выполнения задач
+const dev = gulp.series(copy, watcher);
+
 // Выполнение сценария по умолчанию
-gulp.task("default", copy);
+gulp.task("default", dev);
 
 /*let fs = require("fs");
 
